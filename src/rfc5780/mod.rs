93d3856e77b4c0019ebb17a57ae59ae52a40e4bd
dejano <dejano.with.tie@gmail.com>
2021-02-01 19:34:10 +0100

    rfc5780 attributes

---
+++ src/rfc5780/mod.rs
@@ -0,0 +1,18 @@
+//! [RFC 5780(NAT Behavior Discovery)][RFC 5780] specific components.
+//!
+//! [RFC 5780]: https://tools.ietf.org/html/rfc5780
+use self::attributes::*;
+
+pub mod attributes;
+
+define_attribute_enums!(
+    Attribute,
+    AttributeDecoder,
+    AttributeEncoder,
+    [
+        ChangeRequest,
+        ResponseOrigin,
+        ResponsePort,
+        OtherAddress
+    ]
+);
