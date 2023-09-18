f26e1a98eb53260d3edc4df074d5e9fda0e963d7
Thomas Eizinger <thomas@eizinger.io>
2023-09-19 09:39:27 +1000

    Formatting

--- src/message.rs
+++ src/message.rs
@@ -214,7 +214,8 @@ impl<A: Attribute> Message<A> {
 
     /// Adds the given attribute to the tail of the attributes in the message.
     pub fn add_attribute(&mut self, attribute: impl Into<A>) {
-        self.attributes.push(LosslessAttribute::new(attribute.into()));
+        self.attributes
+            .push(LosslessAttribute::new(attribute.into()));
     }
 }
 
